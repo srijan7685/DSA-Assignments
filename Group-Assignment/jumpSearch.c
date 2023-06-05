78c58689af056e4b0e21597e0c133d1fd29e0f87
Srijan Adhikari <50620478+srijan7685@users.noreply.github.com>
2023-06-05 21:56:33 +0545

    Update jumpSearch.c

--- Group-Assignment/jumpSearch.c
+++ Group-Assignment/jumpSearch.c
@@ -2,11 +2,19 @@
 #include <stdlib.h>
 #include <time.h>
 #include <math.h>
-#include <limits.h>
 
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
 
-// Function to perform jump search
-char lines[] = "************************************************************\n";
 int jumpSearch(int arr[], int n, int target) {
     int step = sqrt(n);
     int prev = 0;
@@ -30,27 +38,12 @@ int jumpSearch(int arr[], int n, int target) {
     return -1;
 }
 
-// Function to perform bubble sort
-void bubbleSort(int arr[], int n) {
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < n - i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
-                // Swap arr[j] and arr[j+1]
-                int temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-            }
-        }
-    }
-}
-
-void generateRandomNumbers(int arr[], int n) { // Function to generate random numbers
+void generateRandomNumbers(int arr[], int n) {
     for (int i = 0; i < n; i++) {
-        arr[i] = rand() % 100; // Generate random numbers between 0 and 99
+        arr[i] = rand() % 100;
     }
 }
 
-// Function to print array elements
 void printArray(int arr[], int n) {
     for (int i = 0; i < n; i++) {
         printf("%d ", arr[i]);
@@ -59,44 +52,48 @@ void printArray(int arr[], int n) {
 }
 
 int main() {
-    srand(time(0));
-    printf("%s", lines);
-    printf("####### Implementation of Jump search Algorithm #######\n");
-    printf("%s", lines);
-    int n; // Number of elements in the array
+    printf("###############################################\n");
+    printf("####### Implementation of Jump Search #########\n");
+    printf("###############################################\n");
+
+    int n;
     printf("Enter the number of elements in the array: ");
     scanf("%d", &n);
 
-    int arr[n]; // Array to store the random numbers
+    int arr[n];
     generateRandomNumbers(arr, n);
 
     printf("Original array: ");
     printArray(arr, n);
 
-    // Perform sorting operation
     bubbleSort(arr, n);
+
     printf("Sorted array: ");
     printArray(arr, n);
 
-    int target; // Element to search for
+    int target;
     printf("Enter the element to search: ");
     scanf("%d", &target);
 
-    // Perform searching operation
-    clock_t start = clock();
-    int index = jumpSearch(arr, n, target);
-    clock_t end = clock();
-    double timeTaken = ((double) (start)) / CLOCKS_PER_SEC / 1000;
-    for (int i = 0; i < n; i++)
+    struct timespec start, end;
+    clock_gettime(CLOCK_MONOTONIC, &start);
 
-        if (index != -1) {
-            printf("Element found at index %d.\n", index);
-        } else {
-            printf("Element not found 1.\n");
-        }
+    int index = jumpSearch(arr, n, target);
+
+    clock_gettime(CLOCK_MONOTONIC, &end);
+    double timeTaken = (end.tv_sec - start.tv_sec) * 1000.0 +
+                       (end.tv_nsec - start.tv_nsec) / 1000000.0;
+
+    if (index != -1) {
+        printf("Element found at index %d.\n", index);
+    } else {
+        printf("Element not found.\n");
+    }
+
     printf("\n");
-    printf("Time taken: %.4f ms.\n", timeTaken);
+    printf("Time taken: %.4f milliseconds.\n", timeTaken);
     printf("Space required: %lu bytes.\n", n * sizeof(int));
-    printf("********************END**************************");
+    printf("###############################################\n");
+
     return 0;
 }
